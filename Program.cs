5121f4497a8036708fbb9b8df93cf7a5110abd03
Mykolas Socevicius <mykolas.soc@gmail.com>
2020-08-19 15:33:04 +0300

    Added funcionality

--- Program.cs
+++ Program.cs
@@ -6,34 +6,34 @@ namespace mainSys
         public class Calendar
         {
             
-            int eventCount;
+            public int EventCount;
             public IList<CalendarEvent> Events = new List<CalendarEvent>();
             public void AddEvent()
             {
                 var newEvent = new CalendarEvent();
+                
                 Console.WriteLine("Enter event name...");
-                newEvent.eventName =  Console.ReadLine();
-                Console.WriteLine("Enter hour...");
-                newEvent.hour = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter minute...");
-                newEvent.minute = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter day...");
-                newEvent.day = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter month(number)...");
-                newEvent.month = Convert.ToInt32(Console.ReadLine());
+                newEvent.EventName =  Console.ReadLine();
                 Console.WriteLine("Enter year...");
-                newEvent.year = Convert.ToInt32(Console.ReadLine());
-                this.Events.Add(newEvent); 
+                int Year = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter month(number)...");
+                int Month = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter day...");
+                int Day = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter hour...");
+                int Hour = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter minute...");
+                int Minute = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter seconds...");
+                int Second = Convert.ToInt32(Console.ReadLine());
+                newEvent.Date = new DateTime(Year, Month, Day, Hour,  Minute, Second);
+                this.Events.Add(newEvent);
+                EventCount ++;
             }
         }
         public class CalendarEvent
         {
-            public  int year;
-            public  int month;
-            public  int day;
-            public  int hour;
-            public  int minute;
-            public  string eventName;
+            public  string EventName;
             public DateTime Date;
 
             
@@ -68,9 +68,50 @@ namespace mainSys
             static void Main()
             {
                 var calendar = new Calendar();
-                calendar.AddEvent();
-                Console.WriteLine("You have added the event " + calendar.Events[0].eventName + " on " + calendar.Events[0].hour + ":" +calendar.Events[0].minute + " " + calendar.Events[0].day + "/" + calendar.Events[0].month + "/" +calendar.Events[0].year );
-                
+                Main2();
+                void Main2(){
+                    Console.WriteLine("Enter 1 to add a new event");
+                    Console.WriteLine("Enter 2 to remove  an event");
+                    Console.WriteLine("Enter 3 to view all events");
+                    Console.WriteLine("Enter 10 to exit program");
+                    int selection = Convert.ToInt32(Console.ReadLine());
+                    switch (selection)
+                    {
+                        case (1):
+                            calendar.AddEvent();
+                            Console.WriteLine("You have added the event " + calendar.Events[0].EventName + " on " + calendar.Events[0].Date);
+                            Main2();
+                            break;
+                        case (2):
+                            for( int i = 0; i < calendar.EventCount; i++)
+                            {
+                                Console.WriteLine(i + ". "+  calendar.Events[i].EventName);
+                                Console.WriteLine("On " + calendar.Events[i].Date);
+                            }
+                            Console.WriteLine("Which event would you like to remove?");
+                            int RmSelection = Convert.ToInt32(Console.ReadLine());
+                            calendar.Events.RemoveAt(RmSelection);
+                            calendar.EventCount--;
+                            Console.WriteLine(calendar.EventCount);
+                            Main2();
+                            break;
+                        case (3):
+                            for( int i = 0; i < calendar.EventCount; i++)
+                            {
+                                Console.WriteLine( calendar.Events[i].EventName);
+                                Console.WriteLine("On " + calendar.Events[i].Date);
+                            }
+                            Main2();
+                            break;
+                        case (10):
+
+                            break;   
+                        default:
+                            Console.WriteLine("None selected");
+                            break;
+
+                    }
+                }
             }
         }
 }
